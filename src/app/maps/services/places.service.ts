fb75a095e20cd91d2b9425de54f4f38ed6382322
Roman Kostyuchenko <elromankos@gmail.com>
2022-02-03 16:17:13 +0100

    Places Service Geolocation invoke
    Injected

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
 
-  constructor() { }
+
+  public userLocation?: [ number, number];
+  //public isLoading;
+
+  get isUserLocationReady ():boolean {
+    return !!this.userLocation;
+  }
+
+  constructor() {
+    this.getUserLocation();
+   }
+
+ //When we have geolocation
+ public async getUserLocation(): Promise<[number, number]> {
+
+  return new Promise ( (resolve, reject) => {
+
+    navigator.geolocation.getCurrentPosition(
+      ( { coords } ) => { 
+        this.userLocation = [ coords.longitude, coords.latitude ];
+        resolve ( this.userLocation );
+      },
+      ( err ) => {
+        alert('Geolocation abcent');
+        console.log(err);
+        reject();
+      }
+    );
+
+
+  }); 
+
+ }
+
+
+
+
 }
